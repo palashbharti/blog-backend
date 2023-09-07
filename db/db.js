7eb306ee71d5d723919e4b5fe76aef2efc7c57ae
palashbharti <reachpalashbharti@gmail.com>
2023-09-07 17:53:07 -0400

    Deployed backend on fly

--- db/db.js
+++ db/db.js
@@ -5,9 +5,16 @@ import PostModel from "./Post.js";
 import CommentModel from "./Comment.js";
 import CommentReplyModel from "./CommentReply.js";
 
+// special vairable only avlbl on Fly
+process.env.DATABASE_URL;
+
+let databaseUrl = "postgres://reachpalashbharti@localhost:5432/blog";
+if (process.env.DATABASE_URL) {
+    databaseUrl = process.env.DATABASE_URL;
+}
 // 8 Use the sequelize import from #3 to connect to the blog database
-const db = new Sequelize("postgres://hackupstate@localhost:5432/blog", {
-	logging: false,
+const db = new Sequelize(databaseUrl, {
+    logging: false,
 });
 
 // 9 Give the post access to the db from 8 through 4
@@ -17,23 +24,23 @@ const CommentReply = CommentReplyModel(db);
 
 //11 connect to db
 const connectToDB = async () => {
-	try {
-		//12A Try and run a query
-		await db.authenticate();
-		//13 If it works, print out we're connected
-		console.log("Connected to database");
+    try {
+        //12A Try and run a query
+        await db.authenticate();
+        //13 If it works, print out we're connected
+        console.log("Connected to database");
 
-		// Comment.belongsTo(Post, { foreignKey: "postID" });
-		Post.hasMany(Comment, { foreignKey: "postID" });
-		Comment.hasMany(CommentReply, { foreignKey: "commentID" });
+        // Comment.belongsTo(Post, { foreignKey: "postID" });
+        Post.hasMany(Comment, { foreignKey: "postID" });
+        Comment.hasMany(CommentReply, { foreignKey: "commentID" });
 
-		//14 Sync the models with the tables in the DB
-		db.sync();
-	} catch (error) {
-		//12B If 12A fails, show an error message
-		console.error(error);
-		console.error("PANIC! DB ISSUE.");
-	}
+        //14 Sync the models with the tables in the DB
+        db.sync();
+    } catch (error) {
+        //12B If 12A fails, show an error message
+        console.error(error);
+        console.error("PANIC! DB ISSUE.");
+    }
 };
 
 //15 Run 11-14 above
